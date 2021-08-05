3089a946e7fe869c2012a84e45e7c81b28623efa
gerasimok <49519944+gerasimok@users.noreply.github.com>
2021-08-05 17:00:10 +0300

    Update smart.php

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'duran.da.grom@gmail.com';                 // Наш логин
-$mail->Password = 'Lvbnhbq1996';                           // Наш пароль от ящика
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
